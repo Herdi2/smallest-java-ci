21a7766082ec594c6faefbc2536b1624ed0c8bc2
Herdi <herdisaleh02@gmail.com>
2025-02-06 14:40:45 +0100

    ssh url

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
+import git
 
 app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook_receiver():
     data = request.json
-    print("data:", data['repository']['ssh_url'])
+    ssh_url = data['repository']['ssh_url']
+    print("data:", ssh_url)
+    git.Git(None).clone(ssh_url)
     print("Received webhook data:", request)
     return jsonify({'message': 'Webhook received successfully'}), 200
 if __name__ == '__main__':
