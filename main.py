48864b48d3ee79af0b693b0f7a27257c5738a7b1
Herdi <herdisaleh02@gmail.com>
2025-02-06 14:23:11 +0100

    Fixed webhook

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook_receiver():
+    data = request.json
     print("Received webhook data:", request)
     return jsonify({'message': 'Webhook received successfully'}), 200
 if __name__ == '__main__':
