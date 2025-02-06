d56ac430448c2855666acce099e957af1ed661ec
Herdi <herdisaleh02@gmail.com>
2025-02-06 14:25:42 +0100

    potato

--- ContinuousIntegrationServer.java
+++ ContinuousIntegrationServer.java
@@ -42,7 +42,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
-        Server server = new Server(8080);
+        Server server = new Server(1234);
         server.setHandler(new ContinuousIntegrationServer()); 
         server.start();
         server.join();
