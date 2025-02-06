766923737c3174515be316effab459dd44b527cc
Herdi <herdisaleh02@gmail.com>
2025-02-06 14:36:28 +0100

    doinng stuff

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook_receiver():
     data = request.json
-    print("data:", data['ssh_url'])
+    print("data:", data['repository']['ssh_url'])
     print("Received webhook data:", request)
     return jsonify({'message': 'Webhook received successfully'}), 200
 if __name__ == '__main__':
