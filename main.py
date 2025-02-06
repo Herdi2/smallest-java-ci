395cab3e3a442114a347c73ae6ee2b5fb5fc72fb
Herdi <herdisaleh02@gmail.com>
2025-02-06 15:00:14 +0100

    potato

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, jsonify
-import git
+import git, pytest
 
 app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook_receiver():
     data = request.json
+    
+    # Get ssh url
     ssh_url = data['repository']['ssh_url']
+    name = data['repository']['name']
     print("data:", ssh_url)
-    git.Git(None).clone(ssh_url)
-    print("Received webhook data:", request)
+    # Clone repo to current directory
+    git.Git(None).clone(ssh_url)    
+    # Run pytest and get results
+    retcode = pytest.main([name + "/test.py"])
+    print(retcode)
     return jsonify({'message': 'Webhook received successfully'}), 200
 if __name__ == '__main__':
     app.run(debug=True)
