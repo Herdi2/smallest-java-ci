ea60922d876aa5671a8a6d97d86b421bf9a9f7bf
Herdi <herdisaleh02@gmail.com>
2025-02-06 14:05:56 +0100

    Test again

--- ContinuousIntegrationServer.java
+++ ContinuousIntegrationServer.java
@@ -1,12 +1,13 @@
+import java.io.BufferedReader;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
  
 import java.io.IOException;
  
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipsejetty.server.Server;
+import org.eclipsejetty.server.Request;
+import org.eclipsejetty.server.handler.AbstractHandler;
 
 /** 
  Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -25,13 +26,17 @@ public class ContinuousIntegrationServer extends AbstractHandler
         baseRequest.setHandled(true);
 
         System.out.println(target);
-
+        BufferedReader r = request.getReader();
+        while (r.ready()) {
+            System.out.println(r.readLine());
+        }
+        
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code
 
-        response.getWriter().println("CI job done");
+        response.getWriter().println("CI job done!");
     }
  
     // used to start the CI server in command line
