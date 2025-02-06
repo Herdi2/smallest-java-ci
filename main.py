61383f8adbb704f52f1e98d4afa5b751fbd12589
Herdi <herdisaleh02@gmail.com>
2025-02-06 15:03:02 +0100

    potato

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import git, pytest
+import git, pytest, os
 
 app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
@@ -15,6 +15,9 @@ def webhook_receiver():
     # Run pytest and get results
     retcode = pytest.main([name + "/test.py"])
     print(retcode)
+    # Cleanup
+    if os.path.exists(name):
+        os.remove("rm -r", name)
     return jsonify({'message': 'Webhook received successfully'}), 200
 if __name__ == '__main__':
     app.run(debug=True)
